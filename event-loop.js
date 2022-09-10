2fd9dc1e3d41346f79ac85e4745d06d9fffcd076
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-09-10 14:08:18 +0600

    first commit

---
+++ event-loop.js
@@ -0,0 +1,10 @@
+console.log(1111111)
+console.log(2222222)
+// setTimeout(() => {
+//     console.log(3333333)
+// })
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+console.log(4444444)
+console.log(5555555)
